2970ae51b23f28e34d6845ea0d480874a2acfd77
Nik Rowell <nik@nikrowell.com>
2021-05-20 18:25:23 -0600

    fix uid now() logic

--- src/misc/uid.js
+++ src/misc/uid.js
@@ -1,9 +1,9 @@
 function now() {
   const time = Date.now();
   const last = now.last || time;
-  return (now.last = time) > last ? time : last + 1;
+  return now.last = (time > last ? time : last + 1);
 }
 
 export function uid(prefix = '', suffix = '') {
-  return prefix + now().toString(36) + suffix;
-}
+  return prefix + now().toString(16) + suffix;
+};
