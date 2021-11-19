ff09250d7dff7c1ce561ab88f3f238dab300c629
Nik Rowell <nik@nikrowell.com>
2021-11-19 10:46:26 -0600

    fix svg implementation

--- src/dom/svg.js
+++ src/dom/svg.js
@@ -1,5 +1,5 @@
 export function svg(strings, ...vars) {
   const string = strings.map((str, i) => `${str}${vars[i] || ''}`).join('');
   const parser = svg.parser || (svg.parser = new DOMParser());
-  return parser.parseFromString(string, 'image/svg+xml').documentElement;
+  return parser.parseFromString(string, 'text/html').body.childNodes[0];
 };
