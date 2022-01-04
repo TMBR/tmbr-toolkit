f6bb01f0d0b9953086b33c1ce84ffedfbf5ad359
Nik Rowell <nik@nikrowell.com>
2022-01-04 15:10:05 -0700

    simplify bind() method to use getOwnPropertyNames and getPrototypeOf vs Reflect

--- src/misc/bind.js
+++ src/misc/bind.js
@@ -1,41 +1,15 @@
-import { isFunction } from '..';
+import { isFunction, isUndefined } from '..';
 
 /**
  * Bind functions to a context or class instance
- * https://www.npmjs.com/package/auto-bind
  *
  * @param  {object}
  * @param  {array}
  * @return {void}
  */
 export function bind(self, fns) {
-
-  if (fns) {
-    fns = [].concat(fns);
-    fns.forEach(fn => isFunction(self[fn]) && (self[fn] = self[fn].bind(self)));
-    return self;
-  }
-
-  const properties = new Set();
-  let object = self.constructor.prototype;
-
-  do {
-
-    for (const key of Reflect.ownKeys(object)) {
-      if (key === 'constructor') continue;
-			properties.add([object, key]);
-		}
-
-	} while ((object = Reflect.getPrototypeOf(object)) && object !== Object.prototype);
-
-	for (const [object, key] of properties) {
-
-		const descriptor = Reflect.getOwnPropertyDescriptor(object, key);
-
-		if (isFunction(descriptor?.value)) {
-      self[key] = self[key].bind(self);
-		}
-	}
-
-	return self;
+  fns = isUndefined(fns)
+    ? Object.getOwnPropertyNames(Object.getPrototypeOf(self))
+    : [].concat(fns);
+  fns.forEach(fn => isFunction(self[fn]) && (self[fn] = self[fn].bind(self)));
 }
