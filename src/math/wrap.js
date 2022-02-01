b3af2daac45ab5995fa3985dd41f7dde4441a841
Nik Rowell <nik@nikrowell.com>
2022-01-31 17:04:28 -0700

    condense wrap()

--- src/math/wrap.js
+++ src/math/wrap.js
@@ -1,4 +1,4 @@
-// import { isArray } from '..';
+import { isArray } from '..';
 
 /**
  * Wrap an index around the given length using the modulo operator
@@ -7,15 +7,11 @@
  *      wrap(3, 3) // 0
  *      wrap(-1, 3) // 2
  *
- * @param  {number} index  Index
- * @param  {number} length Length
- * @return {number}        Looped index
+ * @param  {number} i Index
+ * @param  {number} l Length
+ * @return {number}   Looped index
  */
-export function wrap(index, length) {
-  // isArray(length) && (length = length.length);
-  return index < 0
-    ? (index = length + (index % length))
-    : index >= length
-    ? index % length
-    : index;
+export function wrap(i, l) {
+  isArray(l) && (l = l.length);
+  return i < 0 ? l + (i % l) : i >= l ? i % l : i;
 };
