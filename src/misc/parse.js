3fb2b4ad6561cb63b31ac11f011c64217bbccb36
Nik Rowell <nik@nikrowell.com>
2023-01-29 12:11:30 -0700

    update parse(string) to accept optional defaults to be merged into result

--- src/misc/parse.js
+++ src/misc/parse.js
@@ -1,6 +1,9 @@
-export function parse(string) {
-  try { return new Function(
-    'string',
-    `var object; with (string) { object = ${string} }; return object;`
-  )(string || '{}'); } catch (e) { return {}; }
+export function parse(string, defaults) {
+  let result;
+  try {
+    result = new Function('string', `var object; with (string) { object = ${string} }; return object;`)(string || '{}');
+  } catch (e) {
+    result = {};
+  }
+  return Object.assign(result, defaults);
 };
