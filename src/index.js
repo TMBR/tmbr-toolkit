22825bd7c8adcf0c65854dd7c199de347e549d7a
Nik Rowell <nik@nikrowell.com>
2023-01-28 17:04:48 -0700

    remove observe() in favor of individual io and ro

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ export { findOne } from './dom/findOne';
 export { html } from './dom/html';
 export { on } from './dom/on';
 export { prop } from './dom/prop';
-export { observe } from './dom/observe';
 export { indexOf } from './dom/indexOf';
 export { io } from './dom/io';
 export { rect } from './dom/rect';
