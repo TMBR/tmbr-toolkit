83f1613c2cfc91fe9dc5fe038ae266202a873947
Nik Rowell <nik@nikrowell.com>
2023-02-18 11:29:01 -0700

    simplify ro() implementation

--- src/dom/ro.js
+++ src/dom/ro.js
@@ -1,41 +1,5 @@
-import { isUndefined, isFunction } from '..';
-
-let instance;
-let map;
-
-function handleAll(entries) {
-  entries.forEach(handleOne);
-}
-
-function handleOne(entry) {
-  map.get(entry.target).forEach(fn => {
-    isFunction(fn) && fn(entry);
-  });
-}
-
-export function ro(el, cb) {
-
-  if (isUndefined(instance)) {
-    instance = new ResizeObserver(handleAll);
-    map = new Map();
-  }
-
-  if (!map.has(el)) {
-    instance.observe(el);
-  }
-
-  const callbacks = map.get(el) || [];
-  callbacks.push(cb);
-  map.set(el, callbacks);
-
-  return () => {
-
-    const index = callbacks.indexOf(cb);
-    callbacks.splice(index, 1);
-
-    if (callbacks.length === 0) {
-      instance.unobserve(el);
-      map.delete(el);
-    }
-  }
+export default function ro(el, fn) {
+  const observer = new ResizeObserver(entries => fn(entries[0]));
+  observer.observe(el);
+  return () => observer.disconnect();
 };
