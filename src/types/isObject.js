335aebc8c62ee11f9f10e7c8ac694cfe28c50f64
Nik Rowell <nik@nikrowell.com>
2023-03-05 21:03:46 -0700

    move index.js to root, flatten all utils into ./lib

--- src/types/isObject.js
+++
@@ -1,7 +0,0 @@
-import { isArray } from '..';
-
-console.warn('@tmbr/utils: isObject will be changing to return false for arrays');
-
-export function isObject(value) {
-  return typeof value === 'object' && value !== null; // && !isArray(value);
-};
