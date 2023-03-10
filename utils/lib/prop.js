25da427c79bf7e31261b405a0e6a002576186256
Nik Rowell <nik@nikrowell.com>
2023-03-10 15:30:27 -0500

    update setProperty depcreation warning to only show once

--- utils/lib/prop.js
+++ utils/lib/prop.js
@@ -2,7 +2,10 @@ import { isUndefined } from '..';
 
 // TODO: remove this with 2.0 release
 export function setProperty(target, name, value) {
-  console.warn('@tmbr/utils: setProperty is deprecated in favor of prop(el, name[, value])');
+  if (!setProperty.warned) {
+    console.warn('@tmbr/utils: setProperty is deprecated in favor of prop(el, name[, value])');
+    setProperty.warned = true;
+  }
   if (isUndefined(value)) {
     setProperty(document.documentElement, ...arguments);
   } else {
