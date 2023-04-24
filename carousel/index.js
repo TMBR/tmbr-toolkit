e1736725e48f75bd425ed099fd48382c190d2450
Nik Rowell <nik@nikrowell.com>
2023-04-24 08:32:09 -0600

    add @tmbr/utils imports to carousel

--- carousel/index.js
+++ carousel/index.js
@@ -1,5 +1,6 @@
 import EmblaCarousel from 'embla-carousel';
 import EmblaCarouselClassNames from 'embla-carousel-class-names';
+import { isString, isEmpty, findAll, findAll, attr, bind, fill, html } from '@tmbr/utils';
 
 class Carousel {
 
