10e19bbdb71575eeb57393f9cada79152165225d
Nik Rowell <nik@nikrowell.com>
2023-04-30 16:19:43 -0600

    change toArray to use [].concat vs [value] to be more flexible with existing arrays

--- utils/lib/toArray.js
+++ utils/lib/toArray.js
@@ -1,9 +1,7 @@
-import { isArray } from './isArray.js';
-
 export function toArray(value) {
-  return isArray(value) ? value : (
+  return (
     value instanceof NodeList ||
     value instanceof DOMTokenList ||
     value instanceof HTMLCollection
-  ) ? Array.from(value) : [value];
+  ) ? Array.from(value) : [].concat(value);
 };
