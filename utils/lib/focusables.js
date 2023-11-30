0a67bc1d6525a7edbf3d1d198e499a23c80b8402
Nik Rowell <nik@nikrowell.com>
2023-11-30 13:41:15 -0600

    improve focusables selectors

--- utils/lib/focusables.js
+++ utils/lib/focusables.js
@@ -1,12 +1,21 @@
+const not = {
+  inert: ':not([inert]):not([inert] *)',
+  tabindex: ':not([tabindex^="-"])',
+  disabled: ':not(:disabled)',
+};
+
 export const focusables = [
-  'a[href]',
-  'iframe',
-  'audio[controls]',
-  'video[controls]',
-  'input:not([disabled])',
-  'button:not([disabled])',
-  'select:not([disabled])',
-  'textarea:not([disabled])',
-  '[tabindex="0"]:not([disabled])',
-  '[contenteditable]',
-];
+  `a[href]${not.inert}${not.tabindex}`,
+  `area[href]${not.inert}${not.tabindex}`,
+  `input:not([type="hidden"])${not.inert}${not.tabindex}${not.disabled}`,
+  `input[type="radio"]${not.inert}${not.tabindex}${not.disabled}`,
+  `select${not.inert}${not.tabindex}${not.disabled}`,
+  `textarea${not.inert}${not.tabindex}${not.disabled}`,
+  `button${not.inert}${not.tabindex}${not.disabled}`,
+  `details${not.inert} > summary:first-of-type${not.tabindex}`,
+  `iframe${not.inert}${not.tabindex}`,
+  `audio[controls]${not.inert}${not.tabindex}`,
+  `video[controls]${not.inert}${not.tabindex}`,
+  `[contenteditable]${not.inert}${not.tabindex}`,
+  `[tabindex]${not.inert}${not.tabindex}`,
+]
