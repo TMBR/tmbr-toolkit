4ac55bc9812a3383376299f04734bca5a4d934e6
Nik Rowell <nik@nikrowell.com>
2025-02-06 10:10:50 -0600

    update docs after tree-shaking fixes

--- utils/lib/safe.js
+++ utils/lib/safe.js
@@ -1,5 +1,8 @@
 /**
  * {@link https://wesbos.com/javascript/12-advanced-flow-control/71-async-await-error-handling Async/Await Error Handling}
+ *
+ * @param fn           - try function
+ * @param errorHandler - catch function
  */
 export function safe(fn, errorHandler) {
   return () => fn().catch(errorHandler);
