24668627cfb87cf0b9f8fca0cba9a8412e06ba5f
Nik Rowell <nik@nikrowell.com>
2025-05-12 09:50:35 -0500

    utils@2.7.1 - add optional chaining to fix `isClass` with anonymous functions

--- utils/lib/isClass.js
+++ utils/lib/isClass.js
@@ -4,5 +4,5 @@ import { isFunction } from './isFunction.js';
  * Checks if a function is a class constructor
  */
 export function isClass(fn) {
-  return isFunction(fn) && fn.prototype.constructor.toString().startsWith('class');
+  return isFunction(fn) && fn.prototype?.constructor.toString().startsWith('class');
 };
